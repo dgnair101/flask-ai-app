31cd7640fb6b374fa324a7e1c0ac70013bc07765
dgnair <dgnair101@gmail.com>
2026-05-25 12:33:20 +0530

    Switch to Groq, add vercel config

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, jsonify
-import ollama
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app=Flask(__name__)
+
+client=Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 conversation_history=[]
 
@@ -21,11 +27,11 @@ def chat():
 
     #Send full history to ollama
 
-    response=ollama.chat(model="llama3.2", messages=[{"role":"system", "content":"You are a helpful AI assistant. Be concise and clear"},
+    response=client.chat.completions.create(model="llama-3.3-70b-versatile", messages=[{"role":"system", "content":"You are a helpful AI assistant. Be concise and clear"},
         ] + conversation_history
     )
 
-    ai_response = response["message"]["content"]
+    ai_response = response.choices[0].message.content
 
     #Add AI response to history
     conversation_history.append({
@@ -33,6 +39,8 @@ def chat():
         "content":ai_response
     })
     return jsonify({"response":ai_response})
+
+@app.route("/clear",methods=["POST"])
 def clear():
        conversation_history.clear()
        return jsonify({"status":"cleared"}) 
